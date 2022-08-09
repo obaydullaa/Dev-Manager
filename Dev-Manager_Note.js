85a3a3a3948bbfba1116cfc14137a30c886d5e8e
obaydullaa <cr7obaydul@gmail.com>
2022-08-09 12:24:33 +0600

    Adding Dev Manager App

---
+++ Dev-Manager_Note.js
@@ -0,0 +1,16 @@
+/**
+ * Date: 09-08-2022
+ * =======================
+ * Dev Manager :
+ *  CRUD
+ *  Form Handling
+ *  Remote API server connection and handling
+ *  Routing
+ *  Context API
+ *  Authentication (Registration, Login, Logout)
+ *  Advance login - forgot-password, password-reset, email - sending
+ *  Image-upload, pagination
+ *  Search Functionality
+ *  Securely identify the user (Authorization)
+ * 
+ */
