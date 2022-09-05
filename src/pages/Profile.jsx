1bf1cd17c613903e577013b5d5f40387c7ab1d1e
obaydullaa <cr7obaydul@gmail.com>
2022-09-05 21:25:27 +0600

    Added image upload & show console.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,18 +1,51 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 import { useContext } from 'react'
-import { Col, Container, Row } from 'react-bootstrap';
+import { Button, Col, Container, Row } from 'react-bootstrap';
+import { toast } from 'react-toastify';
 import { AuthContext } from '../context/Auth.context'
 
 function Profile() {
   const {user} = useContext(AuthContext);
-  console.log(user.username)
+  const [file, setFile] = useState(null)
+
+  const handleSubmit = async (evt) => {
+    console.log('hello api')
+    evt.preventDefault();
+    const formData = new FormData()
+    formData.append('files', file)
+    //Send the request to server
+    try{
+      const upload_res = await axios('http://localhost:1337/api/upload',
+       {
+        method: 'POST',
+        data: formData,
+      })
+      console.log(upload_res)
+
+    }catch (err) {
+      console.log(err.response) 
+    }
+  }
+
+  const handleChange = (evt) => {
+    setFile(evt.target.files[0])
+    console.log(evt.target.files[0])
+  }
+  
   return (
     <>
       <Container>
         <Row>
           <div className="user-wrapper">
-            <h2>{user.username}</h2>
-            <h2>{user.email}</h2>
+
+            <form onSubmit={handleSubmit}>
+              <input type='file' accept='image/*' onChange={handleChange} />
+              <Button type='submit'>Upload </Button>
+            </form>
+
+            <h4 className='mt-4'>Name: {user.username}</h4>
+            <h4>Email: {user.email}</h4>
           </div>
         </Row>
       </Container>
