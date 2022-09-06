e06a6ca13e2258d75869ec143173794f6799a69a
obaydullaa <cr7obaydul@gmail.com>
2022-09-07 01:01:38 +0600

    Added image show in display.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,13 +5,16 @@ import { Button, Col, Container, ProgressBar, Row } from 'react-bootstrap';
 import { toast } from 'react-toastify';
 import { AuthContext } from '../context/Auth.context';
 import {axiosPrivateInstance} from '../config/axios.js'
+import Loader from '../components/Loader'
 
-// const uploadPercentage = (total, loaded) => Math.floor((total/loaded) * 100);
+const uploadPercentage = (total, loaded) =>  Math.floor((total / loaded) * 100);
 
 function Profile() {
-  const {user, token} = useContext(AuthContext);
+  const {user, token} = useContext(AuthContext);    
   const [file, setFile] = useState(null)
   const [submitting, setSubmitting] = useState(false)
+  const [percentage, setPercentage] = useState(0)
+  const [imageUrl, setImageUrl] = useState(false)
 
 
   const handleChange = (evt) => {
@@ -40,11 +43,13 @@ function Profile() {
         formData,
         {
           onUploadProgress: (progress) => {
-            console.log(progress)
+            const percentage = uploadPercentage(progress.total, progress.loaded)
+            setPercentage(percentage)
           }
         }
       )
-      console.log(response.data.data.attributes.profilePicture.data.attributes.url)
+
+      setImageUrl(response.data.data.attributes?.profilePicture?.data?.attributes?.url)
       setSubmitting(false)
     }catch (err) {
       console.log(err) 
@@ -57,14 +62,15 @@ function Profile() {
       <Container>
         <Row>
           <div className="user-wrapper">
-            <ProgressBar striped variant="success" now={40} />
+            {imageUrl && <img src={imageUrl} alt='ProfilePicture' />}
+            {submitting && <ProgressBar striped variant="success" now={percentage} /> }
             <h4 className='mt-4'>Name: {username}</h4>
             <h4>Email: {email}</h4>
           </div>
           <form onSubmit={handleSubmit}>
             <label htmlFor='profilePicture'> Profile Picture: </label>
             <input type='file' id='profilePicture' accept='image/*' onChange={handleChange} />
-
+            {percentage > 0 && submitting && <Loader/>}
             <Button type='submit' disabled={submitting}>Upload</Button>
           </form>
         </Row>
